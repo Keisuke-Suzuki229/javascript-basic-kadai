7a1b509472ecfc7c24645e11ed2e5a3638d2be8d
Keisuke-Suzuki229 <154766861+Keisuke-Suzuki229@users.noreply.github.com>
2024-01-26 22:58:12 +1300

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,16 @@
+const PushBtn = document.getElementById('btn');
+
+const h2Text = document.getElementById('text');
+
+
+PushBtn.addEventListener('click',() => {
+
+h2Text.textContent = 'ボタンをクリックしました';
+ 
+
+
+
+  
+
+ 
+});
